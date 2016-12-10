6f25f5d9460d3e8564f87df20830998a37a985ad
muthuvelmurugan75 <muthuvelmurugan75@gmail.com>
2016-12-09 20:42:21 -0800

    Add files via upload

---
+++ Bank.java
@@ -0,0 +1,16 @@
+package com.workout.java;
+
+public class Bank {
+
+	/*public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Bank details=new Bank();
+details.getcustomerdetails();
+	}*/
+
+	protected void getcustomerdetails() {
+		// TODO Auto-generated method stub
+		System.out.println("ur details");
+	}
+
+}
---
+++ Customer.java
@@ -0,0 +1,10 @@
+package com.workout.java;
+
+public class Customer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
