a920defd48abd08d53b8b610ddcf3a6a206ca385
muthuvelmurugan75 <muthuvelmurugan75@gmail.com>
2017-02-06 14:01:32 +0530

    Add files via upload

---
+++ Waiter.java
@@ -0,0 +1,18 @@
+package com.muthuvel.www;
+
+public class Waiter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 Waiter in=new  Waiter();
+		 
+		
+		
+		
+	}
+
+}
+class Waiting{
+	
+}
